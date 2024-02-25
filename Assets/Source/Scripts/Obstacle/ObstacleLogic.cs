485b313f6170c1e7aabf2c55eb4fd1d9f2736c5c
Ivan <gureff.al@yandex.ru>
2024-02-25 21:49:38 +0300

    Add obstacle logic script

---
+++ Assets/Source/Scripts/Obstacle/ObstacleLogic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class ObstacleLogic : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime = 5f;
+    [SerializeField] private float _minSpeedRange;
+    [SerializeField] private float _maxSpeedRange;
+
+    private float _speed;
+
+    private void Start()
+    {
+        _speed = Random.Range(_minSpeedRange, _maxSpeedRange);
+        StartCoroutine(StartDisableObstacle());
+    }
+
+    private void Update()
+    {
+        transform.Translate(
+            new Vector3(0f, _speed * Time.deltaTime, 0f));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            print("Hit player!");
+        }
+    }
+
+    private IEnumerator StartDisableObstacle()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        Destroy(gameObject);
+    }
+
+}
