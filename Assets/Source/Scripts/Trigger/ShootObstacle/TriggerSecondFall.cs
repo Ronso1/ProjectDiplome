19eb3094dbfa6cd84007a16c4168b104caf4de89
Ivan <gureff.al@yandex.ru>
2024-02-26 20:51:01 +0300

    Add fallen triggers

---
+++ Assets/Source/Scripts/Trigger/ShootObstacle/TriggerSecondFall.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TriggerSecondFall : MonoBehaviour
+{
+    [SerializeField] private Transform _respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMove>())
+        {
+            other.transform.position = _respawnPoint.position;
+            other.transform.rotation = _respawnPoint.rotation;
+        }
+    }
+}
