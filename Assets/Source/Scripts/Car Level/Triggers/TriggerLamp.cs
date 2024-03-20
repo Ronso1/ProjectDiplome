ae9705ab095f82d0e01cc5e0f169f1633ae559bd
Ivan <gureff.al@yandex.ru>
2024-03-20 14:35:39 +0300

    Complete develope 1/4 of third part of lvl

--- Assets/Source/Scripts/Car Level/Triggers/TriggerLamp.cs
+++ Assets/Source/Scripts/Car Level/Triggers/TriggerLamp.cs
@@ -1,20 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class TriggerLamp : MonoBehaviour
 {
     [SerializeField] private Light _lamp;
+    [SerializeField] private GameObject _gate;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<SphereCollider>())
         {
             SetActivatedColor();
-            gameObject.SetActive(false);
+            StartCoroutine(GateActions());
         }
     }
 
     private void SetActivatedColor()
     {
         _lamp.color = Color.blue;
+    }
+
+    private IEnumerator GateActions()
+    {
+        _gate.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        _gate.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
