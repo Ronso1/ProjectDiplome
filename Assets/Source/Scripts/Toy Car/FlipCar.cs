2b889c5a7d6ba876ee0d18dad01933173544c1f6
Ivan <gureff.al@yandex.ru>
2024-03-21 15:27:28 +0300

    Improve car level

--- Assets/Source/Scripts/Toy Car/FlipCar.cs
+++ Assets/Source/Scripts/Toy Car/FlipCar.cs
@@ -1,13 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class FlipCar : MonoBehaviour
 {
+    [SerializeField] private float _delay;
+
+    private bool _isEnabled = true;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && _isEnabled)
         {
             transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
             transform.position = new Vector3(transform.position.x, transform.position.y + 5f, transform.position.z);
+            StartCoroutine(DelayRotateButton());
         }
+    }
+
+    private IEnumerator DelayRotateButton()
+    {
+        _isEnabled = false;
+        yield return new WaitForSeconds(_delay);
+        _isEnabled = true;
     }
 }
