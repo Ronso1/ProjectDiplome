c2774d3fc1ad476d8d92f043edaa000516063a50
Ivan <gureff.al@yandex.ru>
2024-04-08 20:50:43 +0300

    Fix logic level loading script

--- Assets/Source/Scripts/CheckLevelComplete.cs
+++ Assets/Source/Scripts/CheckLevelComplete.cs
@@ -6,12 +6,17 @@ public class CheckLevelComplete : MonoBehaviour
     [SerializeField] private List<GameObject> _lockers;
     [SerializeField] private LevelCompleteLogic _levelCompleteLogic;
     [SerializeField] private GameObject _triggerForGate;
+    [SerializeField] private GameObject _startMessage;
 
     private void Start()
     {
         if (_levelCompleteLogic.LevelCompleted != 0)
         {
-            _lockers[_levelCompleteLogic.LevelCompleted - 1].SetActive(false);
+            _startMessage.SetActive(false);
+            for (int i = 0; i < _levelCompleteLogic.LevelCompleted; i++)
+            {
+                _lockers[i].SetActive(false);
+            }      
         }
 
         if (_levelCompleteLogic.LevelCompleted == 4)
